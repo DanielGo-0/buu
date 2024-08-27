c1b56e48fac261c0bfc2ef7248d63aad499c6a32
Nivetha <danielgonzaf@hotmail.com>
2024-08-27 17:57:51 -0300

    last

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -1,17 +1,8 @@
 // app/providers.tsx
 "use client";
 import { ChakraProvider } from "@chakra-ui/react";
-import { CacheProvider } from "@emotion/react";
-import createCache from "@emotion/cache";
 import { ReactNode } from "react";
 
-// Create Emotion cache
-const cache = createCache({ key: "css", prepend: true });
-
 export function Providers({ children }: { children: ReactNode }) {
-  return (
-    <CacheProvider value={cache}>
-      <ChakraProvider>{children}</ChakraProvider>
-    </CacheProvider>
-  );
+  return <ChakraProvider>{children}</ChakraProvider>;
 }
